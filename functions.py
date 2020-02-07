325a02ee4a251b31dca17d74ec3e8267fdfb4dd3
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-07 08:59:01 +0000

    updates made

---
+++ functions.py
@@ -0,0 +1,22 @@
+# The task is to create a functional calcualator:
+# We need it to be able to, add, subtract, divide and multiply, area of a circle, square and triangle
+import math
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def areaofcircle(radius):
+    return round( math.pi * (radius*radius), 2)
+
+def areaofsquare(length, width):
+    return length * width
+
---
+++ math_calc.py
@@ -0,0 +1,17 @@
+from functions import *
+
+print(
+
+add(2,2),
+
+subtract(2,2),
+
+divide(2,2),
+
+multiply(2,2),
+
+areaofcircle(2),
+
+areaofsquare(2,2)
+
+)
